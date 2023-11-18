175fae40fceb571ca4fea65ec0f9d20a6c7c0342
Boateng Paul Okeoghene <opaul816@gmail.com>
2023-11-18 01:27:46 +0000

    add more font files

--- assets/fonts/fonts.js
+++ assets/fonts/fonts.js
@@ -1,27 +1,4 @@
-// import { StyleSheet } from "react-native";
-// const customFonts = {
-//   MontserratRegular: require("./Montserrat-Regular.ttf"),
-//   // Add other custom fonts if needed
-// };
-// export const customFontStyles = StyleSheet.create({
-//   MontserratRegular: {
-//     fontFamily: "MontserratRegular",
-//     fontWeight: "normal",
-//   },
-// });
 import { StyleSheet } from "react-native";
-// import * as Font from "expo-font";
-
-// Define your customFonts object with the correct path to your font file
-// const customFonts = {
-//   MontserratRegular: require("./Montserrat-Regular.ttf"),
-//   // Add other custom fonts if needed
-// };
-
-// Load fonts asynchronously
-// export const loadFontsAsync = async () => {
-//   await Font.loadAsync(customFonts);
-// };
 
 // Export styles for components to use
 export const customFontStyles = StyleSheet.create({
@@ -30,4 +7,6 @@ export const customFontStyles = StyleSheet.create({
     fontWeight: "normal",
   },
   MontserratBold: { fontFamily: "MontserratBold", fontWeight: "normal" },
+  InriaRegular: { fontFamily: "InriaRegular", fontWeight: "normal" },
+  InriaBold: { fontFamily: "InriaBold", fontWeight: "normal" },
 });

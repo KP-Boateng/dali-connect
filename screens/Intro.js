ed8e9b3e61c6a1818ecbc1328791617ad4f6368c
Boateng Paul Okeoghene <opaul816@gmail.com>
2023-11-18 02:14:41 +0000

    modify font of pressable

--- screens/Intro.js
+++ screens/Intro.js
@@ -1,10 +1,11 @@
-import { useNavigation } from "@react-navigation/native";
 import { View, Text, Pressable } from "react-native";
 import tw from "twrnc";
 import { useEffect, useCallback } from "react";
 import { customFontStyles } from "../assets/fonts/fonts";
-// import { useState } from "react";
+
 import * as SplashScreen from "expo-splash-screen";
+//import { onLayoutRootView } from "../assets/fonts/fonts";
+// import { fontsLoaded } from "../assets/fonts/fonts";
 import { useFonts } from "expo-font";
 
 SplashScreen.preventAutoHideAsync();
@@ -48,10 +49,10 @@ const Intro = ({ navigation }) => {
         DALI Connect
       </Text>
       <Pressable
-        style={tw`p-2 bg-white bg-opacity-50 rounded-lg absolute bottom-0 `}
+        style={tw`p-2 bg-white bg-opacity-50 rounded-lg absolute bottom-4 `}
       >
         <Text
-          style={tw.style(customFontStyles.MontserratRegular)}
+          style={tw.style(`text-xl`, customFontStyles.SpaceBold)}
           onPress={() => {
             navigation.navigate("home");
           }}

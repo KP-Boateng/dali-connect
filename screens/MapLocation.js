a303df592d815cbe53c7d2b082d74bbad1d08d20
Boateng Paul Okeoghene <opaul816@gmail.com>
2023-11-26 22:45:25 +0000

    add map location functionality

--- screens/MapLocation.js
+++ screens/MapLocation.js
@@ -1,21 +1,100 @@
+// import React, { useState } from "react";
+// import { View } from "react-native";
+// import tw from "twrnc";
+// import MapView, { Marker } from "react-native-maps";
+// const opencage = require("opencage-api-client");
+
+// const MapLocation = ({ route, navigation }) => {
+//   const { home } = route.params;
+//   const [coordinates, setCoordinates] = useState(null);
+//   const memberLocation = {
+//     latitude: 43.809514, // Replace with the actual latitude of the member's home
+//     longitude: -72.155922, // Replace with the actual longitude of the member's home
+//   };
+
+//   const apiKey = "85003260f2764763810dbe43715e9e07";
+//   // const location = "Paris, France"; // Replace with the location you want to geocode
+
+//   // opencage
+//   //   .geocode({ key: apiKey, q: home })
+//   //   .then((data) => {
+//   //     const { lat, lng } = data.results[0].geometry;
+//   //     const coordinatesObject = { lat, lng };
+//   //     alert(coordinatesObject.lat);
+//   //     setCoordinates(coordinatesObject);
+//   //     // coordinatesObject will be like: { lat: 49.2909409, lng: -123.024879 }
+//   //   })
+//   //   .catch((error) => {
+//   //     console.warn(error.message);
+//   //   });
+
+//   return (
+//     <View style={tw`flex-1 justify-center items-center bg-green-600`}>
+//       <MapView
+//         style={tw`flex-1 w-full h-full`}
+//         initialRegion={{
+//           latitude: memberLocation.latitude,
+//           longitude: memberLocation.longitude,
+//           latitudeDelta: 0.0922,
+//           longitudeDelta: 0.0421,
+//         }}
+//       >
+//         {/* Marker for the member's location */}
+//         <Marker coordinate={memberLocation} title="Member's Home" />
+//       </MapView>
+//     </View>
+//   );
+// };
+
+// export default MapLocation;
+
+import React, { useState, useEffect } from "react";
 import { View } from "react-native";
 import tw from "twrnc";
-import Animation from "../components/Animation";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+const opencage = require("opencage-api-client");
 
-const MapLocation = () => {
+const MapLocation = ({ route, navigation }) => {
+  const { home, name } = route.params;
+  const [coordinates, setCoordinates] = useState(null);
+  const memberLocation = {
+    latitude: 43.809514, // Replace with the actual latitude of the member's home
+    longitude: -72.155922, // Replace with the actual longitude of the member's home
+  };
+
+  const apiKey = "85003260f2764763810dbe43715e9e07";
+
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      try {
+        const response = await opencage.geocode({ key: apiKey, q: home });
+        const { lat, lng } = response.results[0].geometry;
+        const coordinatesObject = { latitude: lat, longitude: lng };
+        setCoordinates(coordinatesObject);
+      } catch (error) {
+        console.warn(error.message);
+      }
+    };
+
+    fetchCoordinates();
+  }, [home, apiKey]);
+
   return (
     <View style={tw`flex-1 justify-center items-center bg-green-600`}>
-      <Animation />
-      {/* <MapView
-        style={tw`flex-1`}
-        initialRegion={{
-          latitude: 37.78825, // Replace with the actual latitude of the member's home
-          longitude: -122.4324, // Replace with the actual longitude of the member's home
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      /> */}
+      {coordinates && (
+        <MapView
+          style={tw`flex-1 w-full h-full`}
+          initialRegion={{
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {/* Marker for the member's location */}
+          <Marker coordinate={coordinates} title={`${name}'s Home`} />
+        </MapView>
+      )}
     </View>
   );
 };
--- screens/ProfileDetail.js
+++ screens/ProfileDetail.js
@@ -72,7 +72,7 @@ const ProfileDetail = ({ route, navigation }) => {
       {details.mentor && (
         <Text
           style={tw.style(
-            `text-white text-center text-xl`,
+            `text-white text-center text-lg`,
             customFontStyles.SpaceRegular
           )}
         >
@@ -82,24 +82,24 @@ const ProfileDetail = ({ route, navigation }) => {
       <ScrollView style={tw`flex-1`}>
         <View style={tw`flex-1 gap-2`}>
           <Text
-            style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+            style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
           >
             Major: {details.major}
           </Text>
           {details.minor && (
             <Text
-              style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+              style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
             >
               Minor: {details.minor}
             </Text>
           )}
           <Text
-            style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+            style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
           >
             Year: {details.year}
           </Text>
           <Text
-            style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+            style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
           >
             Role:{" "}
             {`${details.core ? "(Core)" : ""}${
@@ -110,46 +110,50 @@ const ProfileDetail = ({ route, navigation }) => {
           </Text>
           <View style={tw`flex-row justify-between items-center`}>
             <Text
-              style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+              style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
             >
               Home: {details.home}
             </Text>
             <Pressable
               onPress={() => {
-                navigation.navigate("maplocation");
+                navigation.navigate("maplocation", {
+                  home: details.home,
+                  name: details.name,
+                });
               }}
+              style={{}}
             >
               <Entypo name="location-pin" size={24} color="white" />
             </Pressable>
           </View>
           <Text
-            style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+            style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
           >
             Birthday: {details.birthday}
           </Text>
           <Text
-            style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+            style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
           >
             Favorite Quote: {details.quote}
           </Text>
           <Text
-            style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+            style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
           >
             1st Favorite Thing: {details["favorite thing 1"]}
           </Text>
           <Text
-            style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+            style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
           >
             2nd Favorite Thing: {details["favorite thing 2"]}
           </Text>
           <Text
-            style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+            style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
           >
             3rd Favorite Thing: {details["favorite thing 3"]}
           </Text>
           {details["favorite dartmouth tradition"] && (
             <Text
-              style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+              style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
             >
               Favorite Darmouth Tradition:{" "}
               {details["favorite dartmouth tradition"]}
@@ -157,7 +161,7 @@ const ProfileDetail = ({ route, navigation }) => {
           )}
           {details["fun fact"] && (
             <Text
-              style={tw.style(`text-white text-xl`, customFontStyles.InriaBold)}
+              style={tw.style(`text-white text-lg`, customFontStyles.InriaBold)}
             >
               Fun Fact: {details["fun fact"]}
             </Text>
